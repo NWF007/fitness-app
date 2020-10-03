2392aeb2444ed520bbcc0b8b127c32d9c1f64cb2
Nico <ncfortuin27@gmail.com>
2020-10-03 23:11:04 +0200

    added toolbar, sidenav and tab components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,14 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { LoginComponent } from "./auth/login/login.component";
-import { TrainingComponent } from "./training/training/training.component";
+import { TrainingComponent } from "./training/training.component";
 import { CurrentTrainingComponent } from "./training/current-training/current-training.component";
 import { NewTrainingComponent } from "./training/new-training/new-training.component";
 import { PastTrainingComponent } from "./training/past-training/past-training.component";
 import { WelcomeComponent } from "./welcome/welcome/welcome.component";
+import { HeaderComponent } from "./navigation/header/header.component";
+import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
+import { StopTrainingComponent } from "./training/current-training/stop-training.component";
 
 @NgModule({
   declarations: [
@@ -28,6 +31,9 @@ import { WelcomeComponent } from "./welcome/welcome/welcome.component";
     NewTrainingComponent,
     PastTrainingComponent,
     WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    StopTrainingComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,5 +46,6 @@ import { WelcomeComponent } from "./welcome/welcome/welcome.component";
   ],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [StopTrainingComponent],
 })
 export class AppModule {}
